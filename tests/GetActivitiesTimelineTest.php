51c8ff21dae30cc4a2cbda054682600d4d206b9d
Marek Jelen <marek657@gmail.com>
2022-05-16 15:28:02 +0000

    add activity timeline and activity detail endpoints

---
+++ tests/GetActivitiesTimelineTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Trexima\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
+use Trexima\SriClient\v2\Client;
+
+final class GetActivitiesTimelineTest extends TestCase
+{
+    private $sriClient;
+    private $parameterExtractor;
+
+    public function __construct(?string $name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $cache = new ArrayAdapter();
+        $this->parameterExtractor = new \Trexima\SriClient\MethodParameterExtractor($cache);
+        $this->sriClient = new Client($_ENV['SRI_URL'], $_ENV['SRI_API_KEY'], $this->parameterExtractor, $cache);
+    }
+
+    public function testGetActivitiesTimeline(): void
+    {
+        $activities = $this->sriClient->getActivitiesTimeline();
+        $this->assertGreaterThan(0, count($activities));
+    }
+}
